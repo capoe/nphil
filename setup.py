d84901434d35819a761fab7bae2b4f7c5e386884
capoe <carlpoelking@gmail.com>
2021-03-10 17:40:10 +0000

    Added manifest

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ def get_description():
 if __name__ == "__main__":
     setup(
         name="nphil",
-        version="0.1.0",
+        version="0.1.2",
         author="capoe",
         author_email="carl.poelking@floatlab.io",
         url="https://github.com/capoe/nphil",
